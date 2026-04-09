62e8e03088d1bd194f7cb5dfe14bfef32189d217
Paul Spain <pvspain@gmail.com>
2026-04-09 11:19:57 +1000

    Running commands

--- lib/kv/server.ex
+++ lib/kv/server.ex
@@ -45,6 +45,10 @@ defmodule KV.Server do
     :gen_tcp.send(socket, "UNKNOWN COMMAND\r\n")
   end
 
+  defp write_line(socket, {:error, :not_found}) do
+    :gen_tcp.send(socket, "NOT FOUND\r\n")
+  end
+
   defp write_line(_socket, {:error, :closed}) do
     # The connection was closed, exit politely
     exit(:shutdown)
